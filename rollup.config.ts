1ccf131cb154bdd8058960792a055c02aa61a922
Daniel Holmgren <dtholmgren@gmail.com>
2020-03-13 15:00:04 -0500

    Rollup revamp (#12)
    * revamp rollup config
    
    * bump version
    
    * turn sourcemap back on
    
    * update name
    
    * uncomment babel
    
    * remove externals
    
    * export ipfs:
    
    * bumpa da version

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,35 +1,75 @@
+import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import polyfills from 'rollup-plugin-node-polyfills'
+import typescript from 'rollup-plugin-typescript2'
 
+// Require understands JSON files.
 const pkg = require('./package.json')
 
-export default {
-  input: `src/index.ts`,
-  output: [
-    { file: `dist/${pkg.main}`, name: 'index', format: 'umd', sourcemap: true },
-    { file: `dist/${pkg.module}`, format: 'es', sourcemap: true },
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+const input = 'src/index.ts'
+const name = 'fissionSdk'
 
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
+// External dependencies tell Rollup "it's ok that you can't resolve these modules;
+// don't try to bundle them but rather leave their import statements in place"
+const external = []
+
+const plugins = [
+  // Allow json resolution
+  json(),
+
+  // Compile TypeScript files
+  typescript({ useTsconfigDeclarationDir: true }),
+
+  // Node modules resolution
+  resolve({ browser: true, preferBuiltins: true }),
+
+  // // Let's transpile our own ES6 code into ES5
+  babel({ exclude: 'node_modules/**' }),
+
+  // Most packages in node_modules are legacy CommonJS, so let's convert them to ES
+  commonjs(),
+
+  // Polyfills for node builtins/globals
+  polyfills(),
+]
+
+// browser-friendly UMD build
+const configUMD = {
+  input,
+  output: {
+    name,
+    file: `dist/${pkg.browser}`,
+    format: 'umd',
+    sourcemap: true
+  },
+  plugins,
+  external,
 }
+
+// CommonJS (for Node) and ES module (for bundlers) build.
+// (We could have three entries in the configuration array
+// instead of two, but it's quicker to generate multiple
+// builds from a single configuration where possible, using
+// an array for the `output` option, where we can specify
+// `file` and `format` for each target)
+const configCjsAndEs = {
+  input,
+  output: [
+    {
+      file: `dist/${pkg.main}`,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: `dist/${pkg.module}`,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins,
+  external,
+}
+
+export default [configUMD, configCjsAndEs]
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import auth from './auth'
 import * as ffs from './ffs'
+import ipfs from './ipfs'
 
 export default {
   auth,
   ffs,
+  ipfs,
 }
