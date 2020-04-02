b2c2f7ff34db143c3a5f65aead0424ce48131a32
Daniel Holmgren <dtholmgren@gmail.com>
2020-04-02 13:36:07 -0500

    convert private blobs to buffers

--- src/ffs/private/util.ts
+++ src/ffs/private/util.ts
@@ -28,6 +28,10 @@ export const encryptNode = async (node: PrivateTreeData, keyStr: string): Promis
 }
 
 export const encryptContent = async (content: FileContent, keyStr: string): Promise<Uint8Array> => {
+  if(isBlob(content)){
+    content = await blobToBuffer(content)
+    console.log('content: ', content)
+  }
   const encoded = cbor.encode(content)
   return encrypt(encoded, keyStr)
 }
@@ -46,6 +50,30 @@ export const decryptNode = async (encrypted: Uint8Array, keyStr: string): Promis
 export const decryptContent = async (encrypted: Uint8Array, keyStr: string): Promise<FileContent> => {
   const decrypted = await decrypt(encrypted, keyStr)
   return cbor.decode(decrypted)
+}
+
+const blobToBuffer = async (blob: Blob): Promise<Buffer> => {
+  return new Promise((resolve, reject) => {
+    const fail = () => reject(new Error("Failed to read file"))
+    const reader = new FileReader()
+    reader.addEventListener('load', (e) => {
+      const arrbuf = e?.target?.result || null
+      if(arrbuf === null){
+        fail()
+      }
+      resolve(Buffer.from(arrbuf as ArrayBuffer))
+    })
+    reader.addEventListener('error', () => reader.abort())
+    reader.addEventListener('abort', fail)
+    reader.readAsArrayBuffer(blob)
+  })
+}
+
+const isBlob = (obj: any): obj is Blob => {
+  if (typeof Blob === 'undefined') {
+    return false
+  }
+  return obj instanceof Blob || obj.constructor.name === 'Blob'
 }
 
 export default {
