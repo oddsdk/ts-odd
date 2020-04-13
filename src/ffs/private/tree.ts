4f2b1cc6641af3a9f8fe633d72aac8bc69b34a34
Daniel Holmgren <dtholmgren@gmail.com>
2020-04-13 12:27:58 -0500

    private tree w/ metadata

--- src/ffs/private/tree.ts
+++ src/ffs/private/tree.ts
@@ -15,7 +15,6 @@ export class PrivateTree extends PublicTree {
   }
 
   constructor(links: Links, version: FileSystemVersion, key: string) {
-    console.log("Version: ", version)
     super(links, version)
     this.key = key
     this.static = {
@@ -30,7 +29,6 @@ export class PrivateTree extends PublicTree {
  
   static async empty(version: FileSystemVersion = FileSystemVersion.v1_0_0, key?: string): Promise<PrivateTree> {
     const keyStr = key ? key : await util.genKeyStr()
-    console.log('empty: ', version)
     return new PrivateTree({}, version, keyStr)
   }
 
@@ -41,7 +39,6 @@ export class PrivateTree extends PublicTree {
   static async fromCIDWithKey(cid: CID, parentKey: string): Promise<PrivateTree> {
     const version = await resolver.getVersion(cid, parentKey)
     const { links, key } = await resolver.getTree(cid, parentKey)
-    console.log("From cid with key: ", version)
     return new PrivateTree(links, version, key)
   }
 
