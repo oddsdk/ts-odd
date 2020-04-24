c4bbc2d5bb1fe9b5cad24c9f69dc777377d31c54
Daniel Holmgren <dtholmgren@gmail.com>
2020-04-24 11:52:29 -0500

    simplify exports

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,22 +1,18 @@
-import apiMod from './api'
-import base64Mod from './base64'
-import blobMod from './blob'
-import dnsMod from './dns'
+import api from './api'
+import base64 from './base64'
+import blob from './blob'
+import dns from './dns'
 import constants from './constants'
 import typeChecks from './type-checks'
 import util from './util'
 
+export { api, base64, blob, dns }
 export * from './constants'
 export * from './type-checks'
 export * from './util'
 
-export const api = apiMod
-export const base64 = base64Mod
-export const blob = blobMod
-export const dns = dnsMod
-
 export default {
-  apiMod,
+  api,
   base64,
   blob,
   dns,
