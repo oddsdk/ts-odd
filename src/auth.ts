de565c902263a66da14ad8dc3bb8e421cc868f75
Philipp Krüger <philipp.krueger1@gmail.com>
2021-01-28 13:21:42 +0100

    Add resendVerificationEmail

--- src/auth.ts
+++ src/auth.ts
@@ -5,7 +5,8 @@ import * as common from './common'
 import * as did from './did'
 import * as keystore from './keystore'
 import * as ucan from './ucan/internal'
-import { UCANS_STORAGE_KEY, USERNAME_STORAGE_KEY, Maybe } from './common'
+import * as ucanApi from './ucan'
+import { api, UCANS_STORAGE_KEY, USERNAME_STORAGE_KEY, Maybe } from './common'
 import { Permissions } from './ucan/permissions'
 import { setup } from './setup/internal'
 
@@ -74,4 +75,33 @@ export async function redirectToLobby(
     params
       .map(([k, v]) => encodeURIComponent(k) + "=" + encodeURIComponent(v))
       .join("&")
+}
+
+/**
+ * Ask the fission server to send another verification email to the
+ * user currently logged in.
+ * 
+ * Throws if the user is not logged in.
+ */
+export async function resendVerificationEmail(): Promise<void> {
+  const apiEndpoint = setup.endpoints.api
+
+  const localUcan = await ucan.lookupFilesystemUcan("*")
+  if (localUcan === null) {
+    throw "Could not find your local UCAN"
+  }
+
+  const jwt = await ucanApi.build({
+    audience: await api.did(),
+    issuer: await did.ucan(),
+    proof: ucanApi.encode(localUcan), 
+    potency: null
+  })
+
+  await fetch(`${apiEndpoint}/user/email/resend`, {
+    method: 'POST',
+    headers: {
+      'authorization': `Bearer ${jwt}`
+    }
+  })
 }
