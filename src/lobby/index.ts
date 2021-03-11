3cda0c0d2f79e8f3ef1b771894eaf56cd237f515
Daniel Holmgren <dtholmgren@gmail.com>
2021-03-10 18:12:07 -0700

    fix regex for usernames & allow underscores

--- src/lobby/index.ts
+++ src/lobby/index.ts
@@ -1,11 +1,10 @@
-import * as dataRoot from '../data-root'
 import * as did from '../did'
 import * as ucan from '../ucan'
 import * as ucanInternal from '../ucan/internal'
 import { api } from '../common'
 import { setup } from '../setup/internal'
 
-import { USERNAME_BLOCKLIST } from './blocklist'
+export * from './username'
 
 
 /**
@@ -38,25 +37,6 @@ export async function createAccount(
   }
 }
 
-/**
- * Check if a username is available.
- */
-export async function isUsernameAvailable(
-  username: string
-): Promise<boolean> {
-  const resp = await fetch(`${setup.endpoints.api}/user/data/${username}`)
-  return !resp.ok
-}
-
-/**
- * Check if a username is valid.
- */
-export function isUsernameValid(username: string): boolean {
-  return !username.startsWith("-") &&
-         !username.endsWith("-") &&
-         !!username.match(/[a-zA-Z1-9-]+/) &&
-         !USERNAME_BLOCKLIST.includes(username)
-}
 
 /**
  * Ask the fission server to send another verification email to the
