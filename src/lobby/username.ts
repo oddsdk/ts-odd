30b95e92bc74039f720e1f09a55e44179f24bf91
Daniel Holmgren <dtholmgren@gmail.com>
2021-03-11 13:01:08 -0700

    Fix regex for usernames & allow underscores (#180)
    * fix regex for usernames & allow underscores
    
    * dont allow empty strings
    
    * changelog & version

---
+++ src/lobby/username.ts
@@ -0,0 +1,24 @@
+import { setup } from '../setup/internal'
+
+import { USERNAME_BLOCKLIST } from './blocklist'
+
+/**
+ * Check if a username is available.
+ */
+export async function isUsernameAvailable(
+  username: string
+): Promise<boolean> {
+  const resp = await fetch(`${setup.endpoints.api}/user/data/${username}`)
+  return !resp.ok
+}
+
+/**
+ * Check if a username is valid.
+ */
+export function isUsernameValid(username: string): boolean {
+  return !username.startsWith("-") &&
+         !username.endsWith("-") &&
+         !username.startsWith("_") &&
+         /^[a-zA-Z1-9_-]+$/.test(username) &&
+         !USERNAME_BLOCKLIST.includes(username.toLowerCase())
+}
