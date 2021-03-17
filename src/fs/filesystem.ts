7e93f4494470878e1184769538adc0d8fc7024fc
Steven Vandevelde <icid.asset@gmail.com>
2021-03-17 15:18:17 +0100

    Move fs.storeRootKey to lobby.storeFileSystemRootKey

--- src/fs/filesystem.ts
+++ src/fs/filesystem.ts
@@ -160,13 +160,6 @@ export class FileSystem {
     return fs
   }
 
-  /**
-   * Store the read key for the root `PrivateTree` (ie. `/private`)
-   */
-  static storeRootKey(key: string): Promise<void> {
-    return RootTree.storeRootKey(key)
-  }
-
 
   // DEACTIVATE
   // ----------
