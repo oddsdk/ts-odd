1c84c7e1771ba7b0f282abce2111048c55682feb
Brian Ginsburg <brian@brianginsburg.com>
2021-03-24 17:49:32 -0700

    Bump max size of test URL to 4000 characters
    Name test a bit better.

--- src/common/base64.test.ts
+++ src/common/base64.test.ts
@@ -4,9 +4,9 @@
 import * as fc from 'fast-check';
 import * as base64 from './base64'
 
-test('round trip encode and decode a string', () => {
+test('round trip encode and decode a url', () => {
   fc.assert(
-    fc.property(fc.string({ maxLength: 2000 }), data => {
+    fc.property(fc.string({ maxLength: 4000 }), data => {
       const encodedData = base64.urlEncode(data)
       const decodedData = base64.urlDecode(encodedData)
       expect(data).toEqual(decodedData)
