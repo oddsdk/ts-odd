175fb58e0759358b82d2c85990f1458235c358f7
Steven Vandevelde <icid.asset@gmail.com>
2021-04-07 21:39:36 +0200

    WIP

--- src/common/crypto.ts
+++ src/common/crypto.ts
@@ -9,6 +9,15 @@ export const sha256 = async (buf: ArrayBuffer): Promise<ArrayBuffer> => {
   )
 }
 
+export const sha512 = async (buf: ArrayBuffer): Promise<ArrayBuffer> => {
+  return globalThis.crypto.subtle.digest(
+    {
+        name: "SHA-512",
+    },
+    buf
+  )
+}
+
 export const sha256Str = async(str: string): Promise<string> => {
   const buf = utils.strToArrBuf(str, 8)
   const hash = await sha256(buf)
