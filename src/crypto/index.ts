7c27679963f17a0bc140610434af1d89b7ecb34e
Daniel Holmgren <dtholmgren@gmail.com>
2021-05-07 16:56:18 -0500

    Fix DI issues (#228)
    * fix di issues
    
    * bump version + changelog

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -10,12 +10,47 @@ export const sha256Str = async(str: string): Promise<string> => {
 }
 
 export const hash = {
-  ...impl.hash,
+  sha256: (bytes: Uint8Array): Promise<Uint8Array> =>
+    impl.hash.sha256(bytes),
   sha256Str: sha256Str
 }
+export const aes = {
+  encrypt: (bytes: Uint8Array, key: string): Promise<Uint8Array> => 
+    impl.aes.encrypt(bytes, key),
+  decrypt: (bytes: Uint8Array, key: string): Promise<Uint8Array> => 
+    impl.aes.decrypt(bytes, key),
+  genKeyStr: (): Promise<string> =>
+    impl.aes.genKeyStr(),
+  decryptGCM: (encrypted: string, keyStr: string, ivStr: string): Promise<string>  =>
+    impl.aes.decryptGCM(encrypted, keyStr, ivStr)
+}
 
-export const aes = impl.aes
-export const rsa = impl.rsa
-export const ed25519 = impl.ed25519
-export const keystore = impl.keystore
+export const rsa = {
+  verify: (message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array): Promise<boolean> =>
+    impl.rsa.verify(message, signature, publicKey)
+}
+export const ed25519 = {
+  verify: (message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array): Promise<boolean> =>
+    impl.ed25519.verify(message, signature, publicKey)
+}
 
+export const keystore = {
+  publicReadKey: (): Promise<string> =>
+    impl.keystore.publicReadKey(),
+  publicWriteKey: (): Promise<string> =>
+    impl.keystore.publicWriteKey(),
+  decrypt: (encrypted: string): Promise<string> =>
+    impl.keystore.decrypt(encrypted),
+  sign: (message: string, charSize: number): Promise<string> =>
+    impl.keystore.sign(message, charSize),
+  importSymmKey: (key: string, name: string): Promise<void> =>
+    impl.keystore.importSymmKey(key, name),
+  exportSymmKey: (name: string): Promise<string> =>
+    impl.keystore.exportSymmKey(name),
+  keyExists: (keyName:string): Promise<boolean> =>
+    impl.keystore.keyExists(keyName),
+  getAlg: (): Promise<string> =>
+    impl.keystore.getAlg(),
+  clear: (): Promise<void> =>
+    impl.keystore.clear()
+}
