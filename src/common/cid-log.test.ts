87d78788ae876ae80afd98e4cbc63d7175abf99b
Brian Ginsburg <gins@brianginsburg.com>
2021-05-11 15:52:05 -0700

    Convert cid-log tests to use storage DI
    These tests work individually, but fail as a whole. A bit more investigation
    is needed.

--- src/common/cid-log.test.ts
+++ src/common/cid-log.test.ts
@@ -2,171 +2,133 @@ import * as fc from 'fast-check'
 import CID from 'cids'
 import multihash from 'multihashing-async'
 import * as cidLog from './cid-log'
+import * as storage from '../storage'
 
+// beforeEach(async done => {
+//   return storage.clear()
+//   done()
+// })
 
-// Not tested: clear
-
-// jest.mock('localforage')
-
-beforeAll(async done => {
-  await cidLog.clear()
-  done()
-})
-
-test('simple', async () => {
-  const makeCid = async (data) => {
-    const mhash = await multihash(data, 'sha2-256')
+async function generateCids(data: Uint8Array[]): Promise<string[]> {
+  const promisedCids = data.map(async bytes => {
+    const mhash = await multihash(bytes, 'sha2-256')
     const cid = new CID(1, 'dag-pb', mhash)
     return cid.toString()
-  }
+  })
+  return Promise.all(promisedCids)
+}
 
-  const promisedCids = ['test1', 'test2'].map(str => makeCid(str))
-  const cids = await Promise.all(promisedCids)
+test('gets an empty log when key is missing', async () => {
+  await storage.clear()
 
-  // Sequence promises to keep them in order. Add prepends, so we start from the right. 
-  const doneAdding = cids.reduceRight(async (acc, cid) => {
-    return acc.then(() =>  cidLog.add(cid))
-  }, Promise.resolve())
-
-  // Resolve the ordered promise chain and get the result
-  const log = await doneAdding.then(async () => await cidLog.get())
-
-  expect(log).toEqual(cids)
+  const log = await cidLog.get()
+  expect(log).toEqual([])
 })
 
 test('adds cids and gets an ordered log', async () => {
   fc.assert(
     fc.asyncProperty(
       fc.array(fc.uint8Array({ maxLength: 100 }), { maxLength: 10 }), async data => {
-        await cidLog.clear()
+        await storage.clear()
 
-        const promisedCids = data.map(async bytes => {
-          const mhash = await multihash(bytes, 'sha2-256')
-          const cid = new CID(1, 'dag-pb', mhash)
-          return cid.toString()
-        })
-        const cids: string[] = await Promise.all(promisedCids)
+        // Generate CIDs from test data
+        const cids: string[] = await generateCids(data)
 
-        // localforage.getItem.mockResolvedValue(cids)
+        // Sequence add operations to keep the CIDs in order
+        // Start from the right because add prepends cids
         const doneAdding = cids.reduceRight(async (acc, cid) => {
-          return acc.then(() =>  cidLog.add(cid))
+          return acc.then(() => cidLog.add(cid))
         }, Promise.resolve())
 
+        // Get the log after all CIDs have been added
         const log = await doneAdding.then(async () => await cidLog.get())
 
-        // await Promise.all(cids.map(cid => { console.log(cid); return cidLog.add(cid) }))
-        // await cids.forEach(async cid => { console.log(cid); return await cidLog.add(cid) })
-
-        // const log = await cidLog.get()
-        // console.log(cids + '\n -- \n' + log)
         expect(log).toEqual(cids)
       })
   )
 })
 
-// test('gets cid log', async () => {
-//   fc.assert(
-//     fc.asyncProperty(
-//       fc.array(fc.uint8Array({ maxLength: 100 }), { maxLength: 10 }), async data => {
-//         const promisedCids = data.map(async bytes => {
-//           const mhash = await multihash(bytes, 'sha2-256')
-//           const cid = new CID(1, 'dag-pb', mhash)
-//           return cid.toString()
-//         })
-//         const cids: string[] = await Promise.all(promisedCids)
+test('gets index of a cid', async () => {
+  fc.assert(
+    fc.asyncProperty(
+      fc.array(fc.uint8Array({ maxLength: 100 }), { minLength: 1, maxLength: 10 }), async data => {
+        await storage.clear()
 
-//         // localforage.getItem.mockResolvedValue(cids)
-//         // await Promise.all(cids.map(cid => { console.log(cid); return cidLog.add(cid) }))
-//         await cids.forEach(async cid => { console.log(cid); return await cidLog.add(cid) })
+        const cids: string[] = await generateCids(data)
+        const doneAdding = cids.reduceRight(async (acc, cid) => {
+          return acc.then(() => cidLog.add(cid))
+        }, Promise.resolve())
 
-//         const log = await cidLog.get()
-//         // console.log(cids + '\n -- \n' + log)
-//         expect(log).toEqual(cids)
-//       })
-//   )
-// })
+        const idx = Math.floor(Math.random() * data.length)
+        const cid = cids[idx]
 
-// test('gets an empty log when key is missing', async () => {
-//   localforage.getItem.mockResolvedValue(null)
-//   const log = await cidLog.get()
-//   expect(log).toEqual([])
-// })
+        // Get the index of test cid after all CIDs have been added
+        const index = await doneAdding.then(async () => await cidLog.index(cid.toString()))
 
+        expect(index).toEqual([idx, data.length])
+      })
+  )
+})
 
-// test('gets index of a cid', async () => {
-//   fc.assert(
-//     fc.asyncProperty(
-//       fc.array(fc.uint8Array({ maxLength: 100 }), { minLength: 1, maxLength: 10 }), async data => {
-//         const promisedCids = data.map(async bytes => {
-//           const mhash = await multihash(bytes, 'sha2-256')
-//           const cid = new CID(1, 'dag-pb', mhash)
-//           return cid.toString()
-//         })
-//         const cids: string[] = await Promise.all(promisedCids)
-//         localforage.getItem.mockResolvedValue(cids)
+test('gets the newest cid', async () => {
+  fc.assert(
+    fc.asyncProperty(
+      fc.array(fc.uint8Array({ maxLength: 100 }), { minLength: 1, maxLength: 10 }), async data => {
+        await storage.clear()
 
-//         const idx = Math.floor(Math.random() * data.length)
-//         const cid = cids[idx]
+        const cids: string[] = await generateCids(data)
+        const doneAdding = cids.reduceRight(async (acc, cid) => {
+          return acc.then(() => cidLog.add(cid))
+        }, Promise.resolve())
 
-//         const index = await cidLog.index(cid.toString())
-//         expect(index).toEqual([idx, data.length])
-//       })
-//   )
-// })
+        const cid = cids[0]
 
+        // Get the newest cid after all CIDs have been added
+        const newest = await doneAdding.then(async () => await cidLog.newest())
 
-// test('gets the newest cid', async () => {
-//   fc.assert(
-//     fc.asyncProperty(
-//       fc.array(fc.uint8Array({ maxLength: 100 }), { minLength: 1, maxLength: 10 }), async data => {
-//         const promisedCids = data.map(async bytes => {
-//           const mhash = await multihash(bytes, 'sha2-256')
-//           const cid = new CID(1, 'dag-pb', mhash)
-//           return cid.toString()
-//         })
-//         const cids: string[] = await Promise.all(promisedCids)
-//         localforage.getItem.mockResolvedValue(cids)
+        expect(newest).toEqual(cid)
+      })
+  )
+})
 
-//         const cid = cids[0]
+test('drops older cids when cid log reaches 1000 entries', async () => {
+  fc.assert(
+    fc.asyncProperty(
+      fc.array(fc.uint8Array({ maxLength: 100 }), { minLength: 1001, maxLength: 1003 }), async data => {
+        await storage.clear()
 
-//         const newest = await cidLog.newest()
-//         expect(newest).toEqual(cid)
-//       })
-//   )
-// })
+        const cids: string[] = await generateCids(data)
+        const doneAdding = cids.reduceRight(async (acc, cid) => {
+          return acc.then(() => cidLog.add(cid))
+        }, Promise.resolve())
 
-// test('adds a cid', async () => {
-//   fc.assert(
-//     fc.asyncProperty(
-//       fc.frequency(
-//         { arbitrary: fc.array(fc.uint8Array({ maxLength: 100 }), { minLength: 1, maxLength: 10 }), weight: 100 },
-//         { arbitrary: fc.array(fc.uint8Array({ maxLength: 100 }), { minLength: 1001, maxLength: 1001 }), weight: 1 }
-//       ), async data => {
-//         const promisedCids = data.map(async bytes => {
-//           const mhash = await multihash(bytes, 'sha2-256')
-//           const cid = new CID(1, 'dag-pb', mhash)
-//           return cid.toString()
-//         })
-//         const cids: string[] = await Promise.all(promisedCids)
-//         const cidToAdd = cids[0]
-//         const initialCids = cids.slice(1)
+        // Get the log after all CIDs have been added
+        const log = await doneAdding.then(async () => await cidLog.get())
 
-//         // The call to add needs a mock getItem and setItem. We start off with an initial
-//         // set of a of cids and a mock setItem that gets us access to newItem inside the 
-//         // add implementation.
-//         let log: string[];
-//         localforage.getItem.mockResolvedValue(initialCids)
-//         localforage.setItem.mockImplementation((key, newLog: string[]) =>
-//           log = newLog
-//         )
-//         await cidLog.add(cidToAdd)
+        // Expect only the newest 1000 CIDs to be left
+        expect(log).toEqual(cids.slice(0, 1000))
+      })
+  )
+})
 
-//         // We expect the resulting log to equal the full set of cids up to 1000 CIDs
-//         if (cids.length <= 1000) {
-//           expect(log).toEqual(cids)
-//         } else {
-//           expect(log).toEqual(cids.slice(0, 1000))
-//         }
-//       })
-//   )
-// })
+test('clears the cid log', async () => {
+  fc.assert(
+    fc.asyncProperty(
+      fc.array(fc.uint8Array({ maxLength: 100 }), { maxLength: 5 }), async data => {
+        await storage.clear()
+
+        const cids: string[] = await generateCids(data)
+        const doneAdding = cids.reduceRight(async (acc, cid) => {
+          return acc.then(() => cidLog.add(cid))
+        }, Promise.resolve())
+
+        // Clear the log and get it after all CIDs have been added
+        const log = await doneAdding.then(async () => {
+          await cidLog.clear()
+          return cidLog.get()
+        })
+
+        expect(log).toEqual([])
+      })
+  )
+})
