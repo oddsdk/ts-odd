5a468ff4e7dff4984d59268b75d0728490c3e40c
Brian Ginsburg <gins@brianginsburg.com>
2021-05-11 15:51:19 -0700

    Add in-memory storage DI implementation

--- tests/storage/inMemory.ts
+++ tests/storage/inMemory.ts
@@ -1,85 +1,46 @@
-// import localforage from 'localforage'
-// import { assertBrowser } from '../common/browser'
-
-// let storage: { [key: string]: any} = {}
+/** In memory storage.
+ * This in memory storage implements the methods of localForage.
+ */
 
 export class Storage {
   storage: { [key: string]: any}
 
   constructor() {
-      this.storage = {}
+    this.storage = {}
   }
 
   getItem = <T>(key: string): Promise<T | null> => {
-    return new Promise(
-      (resolve, reject) => {
-        const val = this.storage[key] || null
-        resolve(val)
-      }
-    )
+    return new Promise(resolve => {
+      const val = this.storage[key] || null
+
+      // https://localforage.github.io/localForage/#data-api-getitem
+      // "Even if undefined is saved, null will be returned by getItem(). 
+      // This is due to a limitation in localStorage, and for compatibility 
+      // reasons localForage cannot store the value undefined."
+      const checkedVal = val === undefined ? null : val
+
+      resolve(checkedVal)
+    })
   }
-  
+
   setItem = <T>(key: string, val: T): Promise<T> => {
-    return new Promise(
-      (resolve, reject) => {
-        this.storage[key] = val
-        resolve(val)
-      }
-    )
+    return new Promise(resolve => {
+      this.storage[key] = val
+      resolve(val)
+    })
   }
-  
+
   removeItem = (key: string): Promise<void> => {
-    return new Promise(
-      (resolve, reject) => {
-        delete this.storage[key]
-        resolve()
-      }
-    )
+    return new Promise(resolve => {
+      delete this.storage[key]
+      resolve()
+    })
   }
-  
+
   clear = (): Promise<void> => {
-    return new Promise(
-      (resolve, reject) => {
-        this.storage = {}
-        resolve()
-      }
-    )
+    return new Promise(resolve => {
+      this.storage = {}
+      resolve()
+    })
   }
 }
-
-// export const getItem = <T>(key: string): Promise<T | null> => {
-//   return new Promise(
-//     (resolve, reject) => {
-//       const val = storage[key] || null
-//       resolve(val)
-//     }
-//   )
-// }
-
-// export const setItem = <T>(key: string, val: T): Promise<T> => {
-//   return new Promise(
-//     (resolve, reject) => {
-//       storage[key] = val
-//       resolve(val)
-//     }
-//   )
-// }
-
-// export const removeItem = (key: string): Promise<void> => {
-//   return new Promise(
-//     (resolve, reject) => {
-//       delete storage[key]
-//       resolve()
-//     }
-//   )
-// }
-
-// export const clear = (): Promise<void> => {
-//   return new Promise(
-//     (resolve, reject) => {
-//       storage = {}
-//       resolve()
-//     }
-//   )
-// }
-
