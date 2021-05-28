58980e8802788f762feb5b38ed9ad686a25a71f2
Philipp Krüger <philipp.krueger1@gmail.com>
2021-05-28 21:32:40 +0200

    Niv update & configure yarn with nodejs 16

--- shell.nix
+++ shell.nix
@@ -2,14 +2,14 @@ let
   sources  = import ./nix/sources.nix;
   pkgs     = import sources.nixpkgs  {};
   unstable = import sources.unstable {};
-  # https://github.com/NixOS/nixpkgs/issues/53820#issuecomment-617973476
-  yarn = unstable.yarn.override { nodejs = null; };
+  # https://github.com/NixOS/nixpkgs/issues/53820
+  yarn = unstable.yarn.override { nodejs = unstable.nodejs-16_x; };
 in
 
 pkgs.mkShell {
   buildInputs = [
     yarn
-    unstable.nodejs-15_x
+    unstable.nodejs-16_x
     unstable.niv
   ];
 
