9f4807629f60c5293c9bd383757362d85b9b61e6
Brian Ginsburg <gins@brianginsburg.com>
2021-06-03 14:30:39 -0700

    Add more filesystem api tests
    Upgrade to node 16

--- tests/fs/api.test.ts
+++ tests/fs/api.test.ts
@@ -13,7 +13,6 @@ let ipfs: IPFS | null = null
 let rootKey: string
 
 beforeAll(async () => {
-  rootKey = await crypto.aes.genKeyStr()
   ipfs = await createInMemoryIPFS()
   ipfsConfig.set(ipfs)
 })
@@ -23,111 +22,399 @@ afterAll(async () => {
   await ipfs.stop()
 })
 
-describe("the filesystem api", () => {
-  let fs: FileSystem
+fc.configureGlobal({ numRuns: 10 }) 
 
-  beforeEach(async () => {
-    fs = await FileSystem.empty({
-      localOnly: true,
-      permissions: {
-        fs: {
-          public: [path.root()],
-          private: [path.root()]
-        }
-      },
-      rootKey
+describe("the filesystem api", () => {
+  describe("operates on the private filesystem to", () => {
+    it("write files", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), privateFileContent()), async data => {
+            const filepath = path.file("private", data[0])
+
+            await fs.write(filepath, data[1])
+
+            expect(await fs.exists(filepath)).toEqual(true)
+          })
+      )
+    })
+
+    it("remove what it writes", async () => {
+      const fs = await emptyFilesystem()
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), privateFileContent()), async data => {
+            const filepath = path.file("private", data[0])
+
+            await fs.write(filepath, data[1])
+            await fs.rm(filepath)
+
+            expect(await fs.exists(filepath)).toEqual(false)
+          })
+      )
+    })
+
+    it("read files it writes", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), privateFileContent()), async data => {
+            const filepath = path.file("private", data[0])
+
+            await fs.write(filepath, data[1])
+
+            expect(await fs.read(filepath)).toEqual(data[1])
+          })
+      )
+    })
+
+    it("moves files", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegmentPair(), privateFileContent()), async data => {
+            const fromPath = path.file("private", data[0][0])
+            const toPath = path.file("private", data[0][1])
+
+            await fs.write(fromPath, data[1])
+            await fs.mv(fromPath, toPath)
+            const fromExists = await fs.exists(fromPath)
+            const toExists = await fs.exists(toPath)
+
+            expect(toExists && !fromExists).toEqual(true)
+          })
+      )
+    })
+
+    it("reads moved files", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegmentPair(), privateFileContent()), async data => {
+            const fromPath = path.file("private", data[0][0])
+            const toPath = path.file("private", data[0][1])
+
+            await fs.write(fromPath, data[1])
+            await fs.mv(fromPath, toPath)
+
+            expect(await fs.read(toPath)).toEqual(data[1])
+          })
+      )
+    })
+
+    it("make directories", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          pathSegment(), async data => {
+            const dirpath = path.directory("private", data[0])
+
+            await fs.mkdir(dirpath)
+
+            expect(await fs.exists(dirpath)).toEqual(true)
+          })
+      )
+    })
+
+    it("removes directories it makes", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          pathSegment(), async data => {
+            const dirpath = path.directory("private", data[0])
+
+            await fs.mkdir(dirpath)
+            await fs.rm(dirpath)
+
+            expect(await fs.exists(dirpath)).toEqual(false)
+          })
+      )
+    })
+
+    it("writes files to a directory", async () => {
+      const fs = await emptyFilesystem()
+      const dirpath = path.directory("private", "testDir")
+      await fs.mkdir(dirpath)
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), privateFileContent()), async data => {
+            const filepath = path.file("private", "testDir", data[0])
+
+            await fs.write(filepath, data[1])
+
+            expect(await fs.exists(filepath)).toEqual(true)
+          })
+      )
+    })
+
+    it("lists files written to a directory", async () => {
+      const fs = await emptyFilesystem()
+      const dirpath = path.directory("private", "testDir")
+      await fs.mkdir(dirpath)
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), privateFileContent()), async data => {
+            const filepath = path.file("private", "testDir", data[0])
+
+            await fs.write(filepath, data[1])
+            const listing = await fs.ls(dirpath)
+
+            expect(data[0] in listing).toEqual(true)
+          })
+      )
+    })
+
+    it("moves files into a directory", async () => {
+      const fs = await emptyFilesystem()
+      const dirpath = path.directory("private", "testDir")
+      await fs.mkdir(dirpath)
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegmentPair(), privateFileContent()), async data => {
+            const fromPath = path.file("private", data[0][0])
+            const toPath = path.file("private", "testDir", data[0][1])
+
+            await fs.write(fromPath, data[1])
+            await fs.mv(fromPath, toPath)
+            const fromExists = await fs.exists(fromPath)
+            const toExists = await fs.exists(toPath)
+
+            expect(toExists && !fromExists).toEqual(true)
+          })
+      )
     })
   })
 
-  it("writes to private", async () => {
-    fc.assert(
-      fc.asyncProperty(
-        fc.tuple(pathSegment(), fileContent()), async data => {
-          const filepath = path.file("private", data[0])
-          await fs.write(filepath, data[1])
-          expect(await fs.exists(filepath)).toEqual(true)
-        })
-    )
-  })
 
-  it("removes what it writes to private", async () => {
-    fc.assert(
-      fc.asyncProperty(
-        fc.tuple(pathSegment(), fileContent()), async data => {
-          const filepath = path.file("private", data[0])
-          await fs.write(filepath, data[1])
-          await fs.rm(filepath)
-          expect(await fs.exists(filepath)).toEqual(false)
-        })
-    )
-  })
+  describe("operates on the public filesystem to",() => {
+    it("write files", async () => {
+      const fs = await emptyFilesystem()
 
-  it("reads what it writes to private", async () => {
-    fc.assert(
-      fc.asyncProperty(
-        fc.tuple(pathSegment(), fileContent()), async data => {
-          const filepath = path.file("private", data[0])
-          await fs.write(filepath, data[1])
-          expect(await fs.read(filepath)).toEqual(data[1])
-        })
-    )
-  })
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), publicFileContent()), async data => {
+            const filepath = path.file("public", data[0])
 
+            await fs.write(filepath, data[1])
 
-  it("writes to public", async () => {
-    fc.assert(
-      fc.asyncProperty(
-        fc.tuple(pathSegment(), fileContent()), async data => {
-          const filepath = path.file("public", data[0])
-          await fs.write(filepath, data[1])
-          expect(await fs.exists(filepath)).toEqual(true)
-        })
-    )
-  })
+            expect(await fs.exists(filepath)).toEqual(true)
+          })
+      )
+    })
 
-  it("removes what it writes to public", async () => {
-    fc.assert(
-      fc.asyncProperty(
-        fc.tuple(pathSegment(), fileContent()), async data => {
-          const filepath = path.file("public", data[0])
-          await fs.write(filepath, data[1])
-          await fs.rm(filepath)
-          expect(await fs.exists(filepath)).toEqual(false)
-        })
-    )
-  })
+    it("remove files it writes", async () => {
+      const fs = await emptyFilesystem()
 
-  it("reads what it writes to public", async () => {
-    fc.assert(
-      fc.asyncProperty(
-        fc.tuple(pathSegment(), fileContent()), async data => {
-          const filepath = path.file("public", data[0])
-          await fs.write(filepath, data[1])
-          expect(await fs.read(filepath)).toEqual(data[1])
-        })
-    )
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), publicFileContent()), async data => {
+            const filepath = path.file("public", data[0])
+
+            await fs.write(filepath, data[1])
+            await fs.rm(filepath)
+
+            expect(await fs.exists(filepath)).toEqual(false)
+          })
+      )
+    })
+
+    it("read files it writes", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), publicFileContent()), async data => {
+            const filepath = path.file("public", data[0])
+
+            await fs.write(filepath, data[1])
+
+            expect(await fs.read(filepath)).toEqual(data[1])
+          })
+      )
+    })
+
+    it("moves files", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegmentPair(), publicFileContent()), async data => {
+            const fromPath = path.file("public", data[0][0])
+            const toPath = path.file("public", data[0][1])
+
+            await fs.write(fromPath, data[1])
+            await fs.mv(fromPath, toPath)
+            const fromExists = await fs.exists(fromPath)
+            const toExists = await fs.exists(toPath)
+
+            expect(toExists && !fromExists).toEqual(true)
+          })
+      )
+    })
+
+    it("reads moved files", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegmentPair(), publicFileContent()), async data => {
+            const fromPath = path.file("public", data[0][0])
+            const toPath = path.file("public", data[0][1])
+
+            await fs.write(fromPath, data[1])
+            await fs.mv(fromPath, toPath)
+
+            expect(await fs.read(toPath)).toEqual(data[1])
+          })
+      )
+    })
+
+    it("make directories", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          pathSegment(), async data => {
+            const dirpath = path.directory("public", data[0])
+
+            await fs.mkdir(dirpath)
+
+            expect(await fs.exists(dirpath)).toEqual(true)
+          })
+      )
+    })
+
+    it("removes directories it makes", async () => {
+      const fs = await emptyFilesystem()
+
+      await fc.assert(
+        fc.asyncProperty(
+          pathSegment(), async data => {
+            const dirpath = path.directory("public", data[0])
+
+            await fs.mkdir(dirpath)
+            await fs.rm(dirpath)
+
+            expect(await fs.exists(dirpath)).toEqual(false)
+          })
+      )
+    })
+
+    it("writes files to a directory", async () => {
+      const fs = await emptyFilesystem()
+      const dirpath = path.directory("public", "testDir")
+      await fs.mkdir(dirpath)
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), publicFileContent()), async data => {
+            const filepath = path.file("public", "testDir", data[0])
+
+            await fs.write(filepath, data[1])
+
+            expect(await fs.exists(filepath)).toEqual(true)
+          })
+      )
+    })
+
+    it("lists files written to a directory", async () => {
+      const fs = await emptyFilesystem()
+      const dirpath = path.directory("public", "testDir")
+      await fs.mkdir(dirpath)
+
+      await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegment(), publicFileContent()), async data => {
+            const filepath = path.file("public", "testDir", data[0])
+
+            await fs.write(filepath, data[1])
+            const listing = await fs.ls(dirpath)
+
+            expect(data[0] in listing).toEqual(true)
+          })
+      )
+    })
+
+    it("moves files into a directory", async () => {
+      const fs = await emptyFilesystem()
+      const dirpath = path.directory("public", "testDir")
+      await fs.mkdir(dirpath)
+
+      await await fc.assert(
+        fc.asyncProperty(
+          fc.tuple(pathSegmentPair(), publicFileContent()), async data => {
+            const fromPath = path.file("public", data[0][0])
+            const toPath = path.file("public", "testDir", data[0][1])
+
+            await fs.write(fromPath, data[1])
+            await fs.mv(fromPath, toPath)
+            const fromExists = await fs.exists(fromPath)
+            const toExists = await fs.exists(toPath)
+
+            expect(toExists && !fromExists).toEqual(true)
+          })
+      )
+    })
   })
 })
 
 
 /* Helpers */
 
+const emptyFilesystem = async () => {
+  rootKey = await crypto.aes.genKeyStr()
+  return FileSystem.empty({
+    localOnly: true,
+    permissions: {
+      fs: {
+        public: [path.root()],
+        private: [path.root()]
+      }
+    },
+    rootKey
+  })
+}
+
 const pathSegment = () => {
   return fc.hexaString({ minLength: 1, maxLength: 20 })
 }
 
-const fileContent = () => {
+const pathSegmentPair = () => {
+  return fc.set(
+    fc.hexaString({ minLength: 1, maxLength: 20 }),
+    {minLength: 2, maxLength: 2}
+  )
+}
+
+const privateFileContent = () => {
   return fc.frequency(
     { arbitrary: simpleContent(), weight: 10 },
     { arbitrary: bufferContent(), weight: 1 },
-    { arbitrary: recordContent(), weight: 1 },
+    { arbitrary: recordContent(), weight: 1 }
+  )
+}
+
+const publicFileContent = () => {
+  return fc.frequency(
+    { arbitrary: simpleContent(), weight: 10 },
+    { arbitrary: bufferContent(), weight: 1 }
   )
 }
 
 const simpleContent = () => {
   return fc.frequency(
     { arbitrary: fc.json(), weight: 10 },
-    { arbitrary: fc.string(), weight: 5 },
+    { arbitrary: fc.string({ minLength: 1}), weight: 5 },
     { arbitrary: fc.integer(), weight: 2 },
     { arbitrary: fc.double(), weight: 2 },
     { arbitrary: fc.boolean(), weight: 1 }
