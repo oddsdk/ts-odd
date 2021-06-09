5904b409ee98f9f599587f249de1e7efa27a5409
Brian Ginsburg <gins@brianginsburg.com>
2021-06-09 14:28:47 -0700

    Break apart public and private and refine tests
    Separate the public and private filesystem tests. They behave differently, and
    it seems best to give them their own test suites.
    Align generators with FileContent type.
    Add decoding to read in the public filesystem.

---
+++ tests/fs/api.public.test.ts
@@ -0,0 +1,292 @@
+import * as fc from 'fast-check'
+
+import { IPFS } from 'ipfs-core'
+import { createInMemoryIPFS } from '../helpers/in-memory-ipfs'
+import FileSystem from '../../src/fs/filesystem'
+
+import * as ipfsConfig from '../../src/ipfs'
+import * as path from '../../src/path'
+import * as crypto from '../../src/crypto'
+
+
+let ipfs: IPFS | null = null
+let rootKey: string
+const utf8decoder = new TextDecoder()
+
+beforeAll(async () => {
+  ipfs = await createInMemoryIPFS()
+  ipfsConfig.set(ipfs)
+})
+
+afterAll(async () => {
+  if (ipfs === null) return
+  await ipfs.stop()
+})
+
+fc.configureGlobal({ numRuns: 10 })
+
+describe('the filesystem api', () => {
+  it('write files', async () => {
+    const fs = await emptyFilesystem()
+
+    await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegment(), fileContent()), async data => {
+          const filepath = path.file('public', data[0])
+          const [val] = data[1]
+
+
+          await fs.write(filepath, val)
+
+          expect(await fs.exists(filepath)).toEqual(true)
+        })
+    )
+  })
+
+  it('remove files it writes', async () => {
+    const fs = await emptyFilesystem()
+
+    await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegment(), fileContent()), async data => {
+          const filepath = path.file('public', data[0])
+          const [val] = data[1]
+
+          await fs.write(filepath, val)
+          await fs.rm(filepath)
+
+          expect(await fs.exists(filepath)).toEqual(false)
+        })
+    )
+  })
+
+  it('read files it writes', async () => {
+    const fs = await emptyFilesystem()
+
+    await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegment(), fileContent()), async data => {
+          const filepath = path.file('public', data[0])
+          const [val, type] = data[1]
+
+          await fs.write(filepath, val)
+          const file = await fs.read(filepath)
+          const decodedContent = decode(file, type)
+
+          expect(decodedContent).toEqual(val)
+        })
+    )
+  })
+
+  it('moves files', async () => {
+    const fs = await emptyFilesystem()
+
+    await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegmentPair(), fileContent()), async data => {
+          const [from, to] = data[0]
+          const [val] = data[1]
+          const fromPath = path.file('public', from)
+          const toPath = path.file('public', to)
+
+          await fs.write(fromPath, val)
+          await fs.mv(fromPath, toPath)
+          const fromExists = await fs.exists(fromPath)
+          const toExists = await fs.exists(toPath)
+
+          expect(toExists && !fromExists).toEqual(true)
+        })
+    )
+  })
+
+  it('reads moved files', async () => {
+    const fs = await emptyFilesystem()
+
+    await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegmentPair(), fileContent()), async data => {
+          const [from, to] = data[0]
+          const [val, type] = data[1]
+          const fromPath = path.file('public', from)
+          const toPath = path.file('public', to)
+
+          await fs.write(fromPath, val)
+          await fs.mv(fromPath, toPath)
+
+          const file = await fs.read(toPath)
+          const decodedContent = decode(file, type)
+
+          expect(decodedContent).toEqual(val)
+        })
+    )
+  })
+
+  it('make directories', async () => {
+    const fs = await emptyFilesystem()
+
+    await fc.assert(
+      fc.asyncProperty(
+        pathSegment(), async data => {
+          const dirpath = path.directory('public', data[0])
+
+          await fs.mkdir(dirpath)
+
+          expect(await fs.exists(dirpath)).toEqual(true)
+        })
+    )
+  })
+
+  it('removes directories it makes', async () => {
+    const fs = await emptyFilesystem()
+
+    await fc.assert(
+      fc.asyncProperty(
+        pathSegment(), async data => {
+          const dirpath = path.directory('public', data[0])
+
+          await fs.mkdir(dirpath)
+          await fs.rm(dirpath)
+
+          expect(await fs.exists(dirpath)).toEqual(false)
+        })
+    )
+  })
+
+  it('writes files to a directory', async () => {
+    const fs = await emptyFilesystem()
+    const dirpath = path.directory('public', 'testDir')
+    await fs.mkdir(dirpath)
+
+    await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegment(), fileContent()), async data => {
+          const filepath = path.file('public', 'testDir', data[0])
+          const [val] = data[1]
+
+          await fs.write(filepath, val)
+
+          expect(await fs.exists(filepath)).toEqual(true)
+        })
+    )
+  })
+
+  it('lists files written to a directory', async () => {
+    const fs = await emptyFilesystem()
+    const dirpath = path.directory('public', 'testDir')
+    await fs.mkdir(dirpath)
+
+    await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegment(), fileContent()), async data => {
+          const filepath = path.file('public', 'testDir', data[0])
+          const [val] = data[1]
+
+          await fs.write(filepath, val)
+          const listing = await fs.ls(dirpath)
+
+          expect(data[0] in listing).toEqual(true)
+        })
+    )
+  })
+
+  it('moves files into a directory', async () => {
+    const fs = await emptyFilesystem()
+    const dirpath = path.directory('public', 'testDir')
+    await fs.mkdir(dirpath)
+
+    await await fc.assert(
+      fc.asyncProperty(
+        fc.tuple(pathSegmentPair(), fileContent()), async data => {
+          const [from, to] = data[0]
+          const [val] = data[1]
+          const fromPath = path.file('public', from)
+          const toPath = path.file('public', to)
+
+          await fs.write(fromPath, val)
+          await fs.mv(fromPath, toPath)
+          const fromExists = await fs.exists(fromPath)
+          const toExists = await fs.exists(toPath)
+
+          expect(toExists && !fromExists).toEqual(true)
+        })
+    )
+  })
+})
+
+
+/* Filesystem */
+
+const emptyFilesystem = async () => {
+  rootKey = await crypto.aes.genKeyStr()
+  return FileSystem.empty({
+    localOnly: true,
+    permissions: {
+      fs: {
+        public: [path.root()],
+        private: [path.root()]
+      }
+    },
+    rootKey
+  })
+}
+
+/* Paths */
+
+const pathSegment = () => {
+  return fc.hexaString({ minLength: 1, maxLength: 20 })
+}
+
+const pathSegmentPair = () => {
+  return fc.set(
+    fc.hexaString({ minLength: 4, maxLength: 20 }),
+    { minLength: 2, maxLength: 2 }
+  )
+}
+
+/* File content */
+
+const fileContent = () => {
+  return fc.frequency(
+    { arbitrary: simpleContent(), weight: 10 },
+    { arbitrary: rawFileContent(), weight: 3 },
+  )
+}
+
+const simpleContent = () => {
+  return fc.frequency(
+    { arbitrary: fc.tuple(fc.json(), fc.constant('string')), weight: 10 },
+    { arbitrary: fc.tuple(fc.string({ minLength: 1 }), fc.constant('string')), weight: 5 },
+    { arbitrary: fc.tuple(fc.integer(), fc.constant('number')), weight: 2 },
+    { arbitrary: fc.tuple(fc.double(), fc.constant('number')), weight: 2 },
+    { arbitrary: fc.tuple(fc.boolean(), fc.constant('boolean')), weight: 1 }
+  )
+}
+
+const rawFileContent = () => {
+  return fc.tuple(fc.uint8Array({ minLength: 1 }), fc.constant('rawFileContent'))
+}
+
+const decode = (file, type) => {
+  switch (type) {
+    case 'string':
+      return utf8decoder.decode(file)
+
+    case 'number':
+      return +utf8decoder.decode(file)
+
+    case 'boolean':
+      if (utf8decoder.decode(file) === 'true') {
+        return true
+      } else if (utf8decoder.decode(file) === 'false') {
+        return false
+      } else {
+        return `Invalid boolean string: ${utf8decoder.decode(file)}`
+      }
+
+    case 'rawFileContent':
+      return Uint8Array.from(file as Buffer)
+
+    default:
+      return `Invalid type: ${type}`
+  }
+}
