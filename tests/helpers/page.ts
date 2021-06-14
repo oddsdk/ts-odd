c0c582923ed3d58bc93d84df0d83d22604248587
Philipp Krüger <philipp.krueger1@gmail.com>
2021-06-14 21:55:43 +0200

    Better error for loadWebnativePage

--- tests/helpers/page.ts
+++ tests/helpers/page.ts
@@ -1,7 +1,20 @@
 import path from 'path'
-
+import { promises as fs } from 'fs'
 
 export async function loadWebnativePage(): Promise<void> {
   const htmlPath = path.join(__dirname, '../fixtures/index.html')
   await page.goto(`file://${htmlPath}`)
+  const { isWebnativeLoaded } = await page.evaluate(function () {
+    return {
+      isWebnativeLoaded: window.webnative != null,
+    }
+  })
+  if (!isWebnativeLoaded) {
+    try {
+      await fs.readFile(path.join(__dirname, "../../dist/index.min.js"))
+    } catch {
+      throw new Error("Can't load webpage without a built browser bundle (dist/index.min.js). Please yarn build first.")
+    }
+    throw new Error("Couldn't load webnative in the browser for unknown reasons.")
+  }
 }
