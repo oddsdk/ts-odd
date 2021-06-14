1d8803c5afeb4358dd44c2d1ec52782c23115bde
Philipp Krüger <philipp.krueger1@gmail.com>
2021-06-14 21:16:26 +0200

    Try running tests with nix instead

--- tests/helpers/page.ts
+++ tests/helpers/page.ts
@@ -3,6 +3,5 @@ import path from 'path'
 
 export async function loadWebnativePage(): Promise<void> {
   const htmlPath = path.join(__dirname, '../fixtures/index.html')
-  console.log(__dirname, htmlPath)
   await page.goto(`file://${htmlPath}`)
 }
