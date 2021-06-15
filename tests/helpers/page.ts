5bf0d2c4886108e30ee9f5cc33d1d6c43c9aecee
Philipp Krüger <philipp.krueger1@gmail.com>
2021-06-15 14:52:56 +0200

    Let's try something weird

--- tests/helpers/page.ts
+++ tests/helpers/page.ts
@@ -3,11 +3,25 @@ import { promises as fs } from 'fs'
 
 export async function loadWebnativePage(): Promise<void> {
   const htmlPath = path.join(__dirname, '../fixtures/index.html')
+  await page.setRequestInterception(true)
+  page.on("request", async request => {
+    const url = request.url()
+    if (url.startsWith("file://") && url.endsWith(".js")) {
+      const file = url.replace("file://", "")
+      console.log("responding with file", file)
+      return request.respond({
+        status: 200,
+        headers: {},
+        contentType: "text/javascript",
+        body: await fs.readFile(file, { encoding: "utf-8" })
+      })
+    }
+    return request.continue()
+  })
   await page.goto(`file://${htmlPath}`)
-  const { isWebnativeLoaded, canBeFetched } = await page.evaluate(async function () {
+  const { isWebnativeLoaded } = await page.evaluate(async function () {
     return {
       isWebnativeLoaded: window.webnative != null,
-      canBeFetched: (await fetch("../../dist/index.min.js")).status < 300
     }
   })
   if (!isWebnativeLoaded) {
@@ -16,6 +30,6 @@ export async function loadWebnativePage(): Promise<void> {
     } catch {
       throw new Error("Can't load webpage without a built browser bundle (dist/index.min.js). Please yarn build first.")
     }
-    throw new Error(`Couldn't load webnative in the browser for unknown reasons. (canBeFetched: ${canBeFetched})`)
+    throw new Error("Couldn't load webnative in the browser for unknown reasons.")
   }
 }
