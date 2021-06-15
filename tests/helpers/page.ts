edf3f47adb974497499c389211857fc92e97bef1
Philipp Krüger <philipp.krueger1@gmail.com>
2021-06-15 15:04:31 +0200

    Try waiting for domcontentloaded

--- tests/helpers/page.ts
+++ tests/helpers/page.ts
@@ -3,22 +3,7 @@ import { promises as fs } from 'fs'
 
 export async function loadWebnativePage(): Promise<void> {
   const htmlPath = path.join(__dirname, '../fixtures/index.html')
-  await page.setRequestInterception(true)
-  page.on("request", async request => {
-    const url = request.url()
-    if (url.startsWith("file://") && url.endsWith(".js")) {
-      const file = url.replace("file://", "")
-      console.log("responding with file", file)
-      return request.respond({
-        status: 200,
-        headers: {},
-        contentType: "text/javascript",
-        body: await fs.readFile(file, { encoding: "utf-8" })
-      })
-    }
-    return request.continue()
-  })
-  await page.goto(`file://${htmlPath}`)
+  await page.goto(`file://${htmlPath}`, { waitUntil: 'domcontentloaded' })
   const { isWebnativeLoaded } = await page.evaluate(async function () {
     return {
       isWebnativeLoaded: window.webnative != null,
