a27e013121a67335e1430cfdb728cb6d3403e4b6
Philipp Krüger <philipp.krueger1@gmail.com>
2021-06-15 14:17:19 +0200

    Try something

--- tests/helpers/page.ts
+++ tests/helpers/page.ts
@@ -4,9 +4,10 @@ import { promises as fs } from 'fs'
 export async function loadWebnativePage(): Promise<void> {
   const htmlPath = path.join(__dirname, '../fixtures/index.html')
   await page.goto(`file://${htmlPath}`)
-  const { isWebnativeLoaded } = await page.evaluate(function () {
+  const { isWebnativeLoaded, canBeFetched } = await page.evaluate(async function () {
     return {
       isWebnativeLoaded: window.webnative != null,
+      canBeFetched: (await fetch("../../dist/index.min.js")).status < 300
     }
   })
   if (!isWebnativeLoaded) {
@@ -15,6 +16,6 @@ export async function loadWebnativePage(): Promise<void> {
     } catch {
       throw new Error("Can't load webpage without a built browser bundle (dist/index.min.js). Please yarn build first.")
     }
-    throw new Error("Couldn't load webnative in the browser for unknown reasons.")
+    throw new Error(`Couldn't load webnative in the browser for unknown reasons. (canBeFetched: ${canBeFetched})`)
   }
 }
