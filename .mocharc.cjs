1beb0c2159e6087ca61b15b984e2a5de6c325881
Philipp Krüger <philipp.krueger1@gmail.com>
2021-08-05 16:46:33 +0200

    Forbid pending and it.only in gh actions

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -6,6 +6,8 @@ module.exports = {
         "src/**/*.test.ts",
         "src/*.test.ts",
     ],
+    forbidOnly: process.env.TEST_ENV == "gh-action" ? true : false,
+    forbidPending: process.env.TEST_ENV == "gh-action" ? true : false,
     require: "tests/mocha-hook.ts",
     timeout: 120000,
     loader: "ts-node/esm",
