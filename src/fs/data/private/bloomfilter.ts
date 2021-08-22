bc62c7f122c433f519cc3919c287c67e9f57ff17
Philipp Krüger <philipp.krueger1@gmail.com>
2021-08-22 11:43:11 +0200

    Use xxhash with different seeds for bloom filter

--- src/fs/data/private/bloomfilter.ts
+++ src/fs/data/private/bloomfilter.ts
@@ -1,3 +1,5 @@
+import xxhash from "xxhashjs"
+
 export type BloomFilter = Uint8Array
 
 export interface BloomParameters {
@@ -14,15 +16,15 @@ export function empty(parameters: BloomParameters): BloomFilter {
   return new Uint8Array(Array.from(new Array(parameters.mBytes)))
 }
 
-export function has(hash: ArrayBuffer, filter: BloomFilter, parameters: BloomParameters): boolean {
-  for (const index of indicesFromHash(hash, parameters)) {
+export function has(element: ArrayBuffer, filter: BloomFilter, parameters: BloomParameters): boolean {
+  for (const index of indicesFor(element, parameters)) {
     if (!getBit(filter, index)) return false
   }
   return true
 }
 
-export function add(hash: ArrayBuffer, filter: BloomFilter, parameters: BloomParameters): void {
-  for (const index of indicesFromHash(hash, parameters)) {
+export function add(element: ArrayBuffer, filter: BloomFilter, parameters: BloomParameters): void {
+  for (const index of indicesFor(element, parameters)) {
     setBit(filter, index)
   }
 }
@@ -39,33 +41,8 @@ function getBit(filter: BloomFilter, bitIndex: number): boolean {
   return (filter[byteIndex] & (1 << indexWithinByte)) !== 0
 }
 
-function indicesFromHash(hash: ArrayBuffer, parameters: BloomParameters): Generator<number, void, unknown> {
-  const view = new DataView(hash)
-  return enhancedDoubleHash(
-    view.getUint32(0, true), // little endian
-    view.getUint32(4, true), // little endian
-    parameters.mBytes * 8, // bytes to bits
-    parameters.kHashes
-  )
-}
-
-/** 
- * https://www.ccs.neu.edu/home/pete/pub/bloom-filters-verification.pdf
- * Section 5.2 "Algorithm 2"
- * A.k.a. the "enhanced double hash".
- * 
- * Will take two hashes `a` and `b` (expected ~32bit) and produce `amount` many hashes modulo `modulo`.
- * 
- * These are the indices for a given element in the bloom filter
- */
-function* enhancedDoubleHash(a: number, b: number, modulo: number, amount: number): Generator<number, void, unknown> {
-  let x = a % modulo
-  let y = b & modulo
-  yield x
-  for (let i = 1; i < amount; i++) {
-    x = (x + y) % modulo
-    y = (y + i) % modulo
-    yield x
+function* indicesFor(element: ArrayBuffer, parameters: BloomParameters): Generator<number, void, unknown> {
+  for (let i = 0; i < parameters.kHashes; i++) {
+    yield xxhash.h32(i).update(element).digest().toNumber() % (parameters.mBytes * 8)
   }
-  return
 }
