dcd4029e6c06e3dbbbeb8148eb6a2cd8dfe25968
Philipp Krüger <philipp.krueger1@gmail.com>
2021-10-27 12:03:44 +0200

    Make version mismatch messages configurable

--- src/setup/internal.ts
+++ src/setup/internal.ts
@@ -5,11 +5,25 @@ export type Endpoints = {
   user: string
 }
 
+export type UserMessages = {
+  versionMismatch: {
+    newer(version: string): string
+    older(version: string): string
+  }
+}
+
+type Setup = {
+  debug: boolean
+  endpoints: Endpoints
+  userMessages: UserMessages
+  shouldPin: boolean
+}
+
 
 /**
  * @internal
  */
-export const setup = {
+export const setup: Setup = {
   debug: false,
 
   endpoints: {
@@ -17,6 +31,13 @@ export const setup = {
     apiVersion: "v2",
     lobby: "https://auth.fission.codes",
     user: "fission.name"
+  },
+
+  userMessages: {
+    versionMismatch: {
+      newer: (): string => `Sorry, we can't sync your filesystem with this app. This app only understands older versions of filesystems. Please try to hard refresh this site or let this app's developer know. Feel free to contact Fission support: support@fission.codes`,
+      older: (): string => `Sorry, we can't sync your filesystem with this app. Your filesystem version is out-dated and it needs to be migrated. Use the migration app or talk to Fission support: support@fission.codes`,
+    }
   },
 
   shouldPin: false,
