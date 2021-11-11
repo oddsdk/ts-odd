91d7c9001e4fa64292cc321887c2926b4c5c87ff
Philipp Krüger <philipp.krueger1@gmail.com>
2021-11-11 17:53:57 +0100

    More namefilter attack testing

--- src/fs/data/private/privateNode.test.ts
+++ src/fs/data/private/privateNode.test.ts
@@ -8,7 +8,7 @@ import * as namefilter from "./namefilter.js"
 import * as ratchet from "./spiralratchet.js"
 import * as bloom from "./bloomfilter.js"
 import { ipfsFromContext } from "../../../../tests/mocha-hook.js"
-import { arbitraryFileSystemUsage, FileSystemModel, FileSystemOperation, initialFileSystemModel, runOperation } from "../../../../tests/helpers/fileSystemModel.js"
+import { arbitraryFileSystemUsage, FileSystemModel, FileSystemOperation, FileSystemUsage, initialFileSystemModel, runOperation } from "../../../../tests/helpers/fileSystemModel.js"
 import { Timestamp } from "../common.js"
 
 
@@ -93,7 +93,7 @@ describe("the private node module", () => {
     directory = await privateNode.write(path, content, directory, ctx)
 
     const ref = await privateNode.storeNodeAndAdvance(directory, ctx)
-    
+
     const iamap = await store.getBackingIAMap()
   })
 
@@ -108,15 +108,15 @@ describe("the private node module", () => {
     }
 
     // @ts-ignore
-    let fs: privateNode.PrivateDirectory
+    let fs: privateNode.PrivateDirectory = null
     // @ts-ignore
-    let fsOps: FileSystemOperation[]
+    let model: FileSystemUsage = null
 
     await fc.assert(
       fc.asyncProperty(
         arbitraryFileSystemUsage({ numOperations: 10 }),
-        async ({ state: state, ops }) => {
-          fsOps = ops
+        async ({ state, ops }) => {
+          model = { state, ops }
 
           fs = await privateNode.newDirectory(namefilter.empty(), ctx)
 
@@ -145,21 +145,44 @@ describe("the private node module", () => {
       }
       return result
     }
-    const encoding = "hex"
-    const reducedNamefilter = namefilters.reduce(binaryAnd)
-    const reducedNamefilterEncoded = uint8arrays.toString(reducedNamefilter, encoding)
-    // @ts-ignore
-    const bareNameEncoded = uint8arrays.toString(fs.bareName, encoding)
+    const encode = (namefilter: Uint8Array) => uint8arrays.toString(namefilter, "hex")
+    const chooseRandom = <T>(arr: T[]) => arr[Math.min(arr.length - 1, Math.floor(Math.random() * arr.length))]
+    const unique = <T>(arr: T[]) => Array.from(new Set(arr).values())
+    const rootBareNameEncoded = encode(namefilters.reduce(binaryAnd))
+
+    const minedNamefiltersEncoded = []
+    const bloomFilterFillTarget = 2 // a bare namefilter with 2 inumbers
+    const maxBitCount = bloomFilterFillTarget * bloom.wnfsParameters.kHashes
+    const expectedBitCounts = [maxBitCount, maxBitCount-1]
+
+    tryingAnother: while (minedNamefiltersEncoded.length < 100000) {
+      let namefilter = chooseRandom(namefilters)
+      let bitCount = bloom.countOnes(namefilter)
+      while (!expectedBitCounts.includes(bitCount)) {
+        namefilter = binaryAnd(namefilter, chooseRandom(namefilters))
+        bitCount = bloom.countOnes(namefilter)
+        if (bitCount <= (bloomFilterFillTarget - 1) * bloom.wnfsParameters.kHashes) {
+          continue tryingAnother
+        }
+      }
+      minedNamefiltersEncoded.push(encode(namefilter))
+    }
+
+    const minedNamefiltersEncodedUnique = unique(minedNamefiltersEncoded)
+
+    const actualBareNamesEncoded = Array.from(store.getMap().entries())
+      .map(([ref, entry]) => encode(privateNode.nodeFromCbor(entry.block).bareName))
+
+    console.log(actualBareNamesEncoded.map(b => bloom.countOnes(uint8arrays.fromString(b, "hex"))))
+
+    const bareNameHits = minedNamefiltersEncodedUnique.filter(namefilter => actualBareNamesEncoded.includes(namefilter))
 
     console.log("ran these filesystem operations:")
-    // @ts-ignore
-    fsOps.forEach(op => console.log(` - ${JSON.stringify(op)}`))
-    console.log("resulting namefilters:")
-    namefilters.forEach(filter => console.log(` - ${uint8arrays.toString(filter, encoding)}`))
-    console.log("binary and of all above:\n", reducedNamefilterEncoded)
-    console.log("bare namefilter of root directory:\n", bareNameEncoded)
-    console.log("Are they the same?", bareNameEncoded === reducedNamefilterEncoded)
-    
+    model.ops.forEach(op => console.log(` - ${JSON.stringify(op)}`))
+    console.log(`resulting in ${namefilters.length} namefilter(s)`)
+    console.log(`mined ${minedNamefiltersEncodedUnique.length} additional namefilters, of which ${bareNameHits.length} are actual hits.`)
+    bareNameHits.forEach(namefilter => console.log(` - ${namefilter}`))
+    console.log(`subdirectories: `, fs.links.length)
   })
 
   it("runs filesystem operations as modeled on ipfs", async function () {
