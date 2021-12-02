89328cd650b389d67e6d1ebe66373069fdfcbfec
Steven Vandevelde <icid.asset@gmail.com>
2021-12-02 18:17:27 +0100

    encodeURIComponent

--- src/lobby/index.ts
+++ src/lobby/index.ts
@@ -81,7 +81,10 @@ export async function resendVerificationEmail(): Promise<{ success: boolean }> {
  * copying the soft links into their private filesystem.
  */
 export function shareLink(details: ShareDetails): string {
-  return setup.endpoints.lobby + "/#/share/" + details.sharedBy.username + "/" + details.shareId + "/"
+  return setup.endpoints.lobby +
+    "/#/share/" +
+    encodeURIComponent(details.sharedBy.username) + "/" +
+    encodeURIComponent(details.shareId) + "/"
 }
 
 
