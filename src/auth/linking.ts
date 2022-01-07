53e3cb93e6f37441c13297facdbb4694809838f7
James Walker <walkah@walkah.net>
2022-01-07 14:43:24 -0500

    adding some comments for clarification

--- src/auth/linking.ts
+++ src/auth/linking.ts
@@ -29,6 +29,8 @@ const ls: LinkingState = {
 const resetLinkingState = () => {
   ls.sessionKey = null
   ls.temporaryRsaPair = null
+  ls.role = null
+  ls.step = null
 }
 
 const nextStep = () => {
@@ -97,6 +99,14 @@ const handleConsumerMessage = async (message: string): Promise<void> => {
 }
 
 // 🔗 Device Linking Steps 
+
+/**
+ * CONSUMER: BROADCAST
+ * 
+ * This is the CONSUMER first step (after opening the channel) where we 
+ * broadcast a temporary public key. This key is then published on channel as 
+ * the throwaway DID key.
+ */
 export const sendTemporaryExchangeKey = async (): Promise<void> => {
   const cfg = config.normalize()
 
@@ -108,6 +118,13 @@ export const sendTemporaryExchangeKey = async (): Promise<void> => {
   nextStep()
 }
 
+/**
+ * state: PRODUCER: BROADCAST
+ * 
+ * This should be called by the PRODUCER upon receiving the throwaway DID key.
+ * 
+ * @param didThrowaway 
+ */
 export const sendSessionKey = async (didThrowaway: string): Promise<void> => {
   ls.sessionKey = await aes.makeKey({ alg: SymmAlg.AES_GCM, length: 256 })
 
@@ -141,6 +158,15 @@ export const sendSessionKey = async (didThrowaway: string): Promise<void> => {
   nextStep()
 }
 
+/**
+ * CONSUMER: NEGOTIATION
+ * 
+ * The Consumer receives the session key and validates the encrypted UCAN. 
+ * Upon success, returns a 6-digit pin code to be use for the user challenge.
+ * 
+ * @param data 
+ * @returns pin
+ */
 export const handleSessionKey = async (data: any): Promise<Uint8Array | null> => {
   if (!ls.temporaryRsaPair || !ls.temporaryRsaPair.privateKey) return null
 
@@ -188,14 +214,19 @@ export const handleSessionKey = async (data: any): Promise<Uint8Array | null> =>
     throw new Error("Closed UCAN session key does not match the one we already have")
   }
 
-  nextStep()
-
   return new Uint8Array(utils.randomBuf(6)).map(n => {
     return n % 10
   })
 }
 
-
+/**
+ * CONSUMER: NEGOTIATION (response)
+ * 
+ * Encrypt and publish the CONSUMER did and generated pin for verification by the PRODUCER.
+ * 
+ * @param pin 
+ * @returns 
+ */
 export const sendUserChallenge = async (pin: Uint8Array): Promise<void> => {
   if (!ls.sessionKey) return
   
@@ -214,6 +245,14 @@ export const sendUserChallenge = async (pin: Uint8Array): Promise<void> => {
   nextStep()
 }
 
+/**
+ * PRODUCER: NEGOTIATION
+ * 
+ * PRODUCER receives the DID & challenge PIN from the CONSUMER.
+ * 
+ * @param data 
+ * @returns 
+ */
 export const handleUserChallenge = async (data: any): Promise<string | null> => {
   if (!ls.sessionKey) return null
 
@@ -232,10 +271,28 @@ export const handleUserChallenge = async (data: any): Promise<string | null> => 
   nextStep()
 }
 
+/**
+ * PRODUCER: DELEGATION
+ * 
+ * This step is user initiated, create the UCAN with delegate rights for the CONSUMER (and a WNFS read key).
+ * 
+ * @param data 
+ * @returns 
+ */
 export const sendReadKeyAndUcan = async (data: any): Promise<null> => {
   return null
 }
 
+/**
+ * CONSUMER: DELEGATION
+ * 
+ * Receive the encrypted UCAN & readKey and store them.
+ * 
+ * @param readKey 
+ * @param ucan 
+ * @param username 
+ * @returns 
+ */
 export const handleLinkedDevice = async (readKey: string, ucan: string, username: string): Promise<null> => {
   //
   return null
