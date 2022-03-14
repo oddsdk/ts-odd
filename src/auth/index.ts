59287f712f47baf2215f27b029099920cc5c1b5b
Brian Ginsburg <gins@brianginsburg.com>
2022-03-14 09:44:08 -0700

    Fix account registration exports
    Destructuring the implemenation and exporting its functions does not
    work because it only happens when the module evaluates. Instead, we can
    export wrapped versions of the implementation functions.

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,7 +1,16 @@
 import { impl } from "./implementation.js"
 
-const {  isUsernameAvailable, isUsernameValid, register } = impl
+export const register = async (options: { username: string; email: string }): Promise<{ success: boolean }> => {
+  return impl.register(options)
+}
 
-export { isUsernameAvailable, isUsernameValid, register }
+export const isUsernameValid = async (username: string): Promise<boolean> => {
+  return impl.isUsernameValid(username)
+}
+
+export const isUsernameAvailable = async (username: string): Promise<boolean> => {
+  return impl.isUsernameAvailable(username)
+}
+
 export { AccountLinkingRequestor, createConsumer as createRequestor } from "./linking/consumer.js"
 export { AccountLinkingProvider, createProducer as createProvider } from "./linking/producer.js"
