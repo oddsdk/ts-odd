641f5f4ab6b22940facd1f0a93ac3f3fea53e264
appcypher <appcypher@outlook.com>
2022-04-01 10:42:33 +0100

    Root DID Lookup Retry (#367)
    * Implement DID lookup retry
    
    * Fix corner case (when `did.root` throws) issue

--- src/auth/channel.ts
+++ src/auth/channel.ts
@@ -19,7 +19,7 @@ export type ChannelData = string | ArrayBufferLike | Blob | ArrayBufferView
 export const createWssChannel = async (options: ChannelOptions): Promise<Channel> => {
   const { username, handleMessage } = options
 
-  const rootDid = await await did.root(username).catch(() => null)
+  const rootDid = await waitForRootDid(username)
   if (!rootDid) {
     throw new LinkingError(`Failed to lookup DID for ${username}`)
   }
@@ -40,6 +40,30 @@ export const createWssChannel = async (options: ChannelOptions): Promise<Channel
     send,
     close
   }
+}
+
+const waitForRootDid = async (username: string): Promise<string | null> => {
+  return new Promise((resolve) => {
+    const maxTries = 3
+    let tries = 0
+    let rootDid: string | null = null
+
+    const rootDidInterval = setInterval(async () => {
+      rootDid = await did.root(username).catch((e) => {
+        clearInterval(rootDidInterval)
+        throw e
+      })
+
+      if (!rootDid && tries < maxTries) {
+        console.log("Could not fetch root DID. Retrying")
+        tries++
+        return
+      }
+
+      clearInterval(rootDidInterval)
+      resolve(rootDid)
+    }, 2000)
+  })
 }
 
 const waitForOpenConnection = async (socket: WebSocket): Promise<void> => {
