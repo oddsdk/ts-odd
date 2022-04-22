49fa45d997a6476506d4e04541db716d4847c06f
Brian Ginsburg <gins@brianginsburg.com>
2022-04-22 13:17:12 -0700

    WIP: Convert IPFS worker to TypeScript

--- scripts/build-ipfs-worker.js
+++ scripts/build-ipfs-worker.js
@@ -5,12 +5,12 @@ console.log("📦 bundling ipfs and ipfs worker...")
 const workerOutfile = "lib/workers/ipfs.worker.js"
 const ipfsOutfile = "lib/workers/ipfs.min.js"
 
-esbuild.buildSync({
-  entryPoints: ["src/workers/ipfs.worker.js"],
-  outfile: workerOutfile,
-  bundle: true,
-  target: "es2020"
-})
+// esbuild.buildSync({
+//   entryPoints: ["src/workers/ipfs.worker.js"],
+//   outfile: workerOutfile,
+//   bundle: true,
+//   target: "es2020"
+// })
 
 esbuild.buildSync({
   entryPoints: ["src/workers/ipfs.min.js"],
