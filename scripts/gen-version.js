626979cbb8bc8c26c87504163a6adcd520def8ba
Philipp Krüger <philipp.krueger1@gmail.com>
2022-07-27 18:29:17 +0200

    Also generate a version for the installed wnfs

--- scripts/gen-version.js
+++ scripts/gen-version.js
@@ -3,7 +3,9 @@ import { promises as fs } from "fs"
 fs.readFile("package.json", { encoding: "utf-8" }).then(pkg => {
     const pkgJson = JSON.parse(pkg)
     const version = pkgJson.version
-    const versionModule = `export const VERSION = ${JSON.stringify(version, null, 4)}` + "\n"
+    let versionModule = ""
+    versionModule += `export const VERSION = ${JSON.stringify(version, null, 4)}\n`
+    versionModule += `export const WASM_WNFS_VERSION = ${JSON.stringify(pkgJson.dependencies.wnfs, null, 4)}\n`
     return fs.writeFile("src/common/version.ts", versionModule, { encoding: "utf-8" })
 }).catch(e => {
     console.error(`There was an issue while trying to generate a "src/common/version.ts" file: ${e.message}\n${e}`)
--- src/common/version.ts
+++ src/common/version.ts
@@ -1,1 +1,2 @@
 export const VERSION = "0.33.0"
+export const WASM_WNFS_VERSION = "*"
