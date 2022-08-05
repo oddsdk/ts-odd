52353df930b67d6734b15be99711c4729502e8aa
Brian Ginsburg <gins@brianginsburg.com>
2022-08-05 11:25:21 -0700

    Use DI transformUsername in toInternalUsername

--- src/auth/username.ts
+++ src/auth/username.ts
@@ -1,8 +1,8 @@
 import * as crypto from "../crypto/index.js"
-import { transformUsername } from "./internal.js"
+import { impl as auth } from "./implementation.js"
 
 export const toInternalUsername = async (username: string): Promise<string> => {
-  const { username: uname, hash } = transformUsername(username)
+  const { username: uname, hash } = auth.transformUsername(username)
 
   if (hash) {
     return await crypto.sha256Str(uname)
