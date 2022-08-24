6ee6dddafccb158ea16381a95adc48efced563cd
Steven Vandevelde <icid.asset@gmail.com>
2022-08-24 11:46:05 +0200

    Remove puppeteer

--- tests/mocha-hook.ts
+++ tests/mocha-hook.ts
@@ -1,14 +1,11 @@
 import type { IPFS } from "ipfs-core-types"
 import * as mocha from "mocha"
-import puppeteer from "puppeteer"
 
 import { createInMemoryIPFS } from "./helpers/in-memory-ipfs.js"
 import * as ipfsConfig from "../src/ipfs/config.js"
 
 declare module "mocha" {
   export interface Context {
-    browser: puppeteer.Browser
-    page?: puppeteer.Page
     ipfs: IPFS
   }
 }
@@ -17,14 +14,12 @@ export const mochaHooks = {
 
   // All
 
-  beforeAll: async function(this: mocha.Context) {
-    this.browser = await puppeteer.launch()
+  beforeAll: async function (this: mocha.Context) {
     this.ipfs = await createInMemoryIPFS()
     ipfsConfig.set(this.ipfs)
   },
 
-  afterAll: async function(this: mocha.Context) {
-    if (this.browser != null) await this.browser.close()
+  afterAll: async function (this: mocha.Context) {
     if (this.ipfs != null) await this.ipfs.stop()
   },
 
@@ -33,7 +28,7 @@ export const mochaHooks = {
   // beforeEach: async function(this: mocha.Context) {
   // },
 
-  afterEach: async function(this: mocha.Context) {
+  afterEach: async function (this: mocha.Context) {
     if (this.page != null) {
       await this.page.close()
     }
@@ -43,25 +38,6 @@ export const mochaHooks = {
 
 function errorContext(functionName: string) {
   return `Called "${functionName}" without a mocha test context. Make sure to run your tests as "it(..., async function(){}" and to provide "this": "${functionName}(this)"`
-}
-
-export const browserFromContext = (that: mocha.Context): puppeteer.Browser => {
-  const browser = that?.test?.ctx?.browser
-  if (browser == null) {
-    throw new Error(errorContext("browserFromContext"))
-  }
-  return browser
-}
-
-export const pageFromContext = async (that: mocha.Context): Promise<puppeteer.Page> => {
-  const browser = browserFromContext(that)
-  const page = await browser.newPage()
-  const ctx = that?.test?.ctx
-  if (ctx == null) {
-    throw new Error(errorContext("pageFromContext"))
-  }
-  ctx.page = page
-  return page
 }
 
 export const ipfsFromContext = (that: mocha.Context): IPFS => {
