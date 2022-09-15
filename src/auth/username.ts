9f809431e99127b2c60733c8c31ba96340b347ba
Brian Ginsburg <gins@brianginsburg.com>
2022-09-15 14:49:26 -0700

    Add safe character check for unhashed usernames

--- src/auth/username.ts
+++ src/auth/username.ts
@@ -1,5 +1,6 @@
 import * as crypto from "../crypto/index.js"
 import { impl as auth } from "./implementation.js"
+import { USERNAME_BLOCKLIST } from "../lobby/blocklist.js"
 
 export const toGlobalUsername = async (username: string): Promise<string> => {
   const { username: uname, hash } = auth.transformUsername(username)
@@ -9,6 +10,21 @@ export const toGlobalUsername = async (username: string): Promise<string> => {
   if (hash) {
     return (await crypto.sha256Str(normalizedUsername)).slice(0, 32)
   } else {
+    if (!isUsernameSafe(uname)) {
+      throw new Error("Invalid username. Please consider hashing username if it contains characters that are not URL safe.")
+    }
+
     return uname
   }
+}
+
+/**
+ * Check if a username is URL and DNS safe
+ */
+ export function isUsernameSafe(username: string): boolean {
+  return !username.startsWith("-") &&
+         !username.endsWith("-") &&
+         !username.startsWith("_") &&
+         /^[a-zA-Z0-9_-]+$/.test(username) &&
+         !USERNAME_BLOCKLIST.includes(username.toLowerCase())
 }
