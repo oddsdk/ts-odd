130795d8f1281c5a7ff9e9be06805941b318d505
Steven Vandevelde <icid.asset@gmail.com>
2022-11-21 15:29:32 +0100

    Add some comments to the auth implementation type

--- src/components/auth/implementation.ts
+++ src/components/auth/implementation.ts
@@ -8,12 +8,19 @@ import { Session } from "../../session.js"
 export type Implementation<C> = {
   type: string,
 
+  // Function that produces a `Session`
   activate: (components: C, authenticatedUsername: Maybe<string>, config: Configuration) => Promise<Maybe<Session>>
-  canDelegateAccount: (username: string) => Promise<boolean>
-  createChannel: (options: ChannelOptions) => Promise<Channel>
-  delegateAccount: (username: string, audience: string) => Promise<Record<string, unknown>>
+
+  // Account creation
   isUsernameAvailable: (username: string) => Promise<boolean>
   isUsernameValid: (username: string) => Promise<boolean>
-  linkDevice: (username: string, data: Record<string, unknown>) => Promise<void>
   register: (options: { username: string; email?: string }) => Promise<{ success: boolean }>
+
+  // Account delegation
+  canDelegateAccount: (username: string) => Promise<boolean>
+  delegateAccount: (username: string, audience: string) => Promise<Record<string, unknown>>
+  linkDevice: (username: string, data: Record<string, unknown>) => Promise<void>
+
+  // Primitives
+  createChannel: (options: ChannelOptions) => Promise<Channel>
 }
