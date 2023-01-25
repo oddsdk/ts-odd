6b45349362c3ec482644ccec2bd4ebd3d6acf367
Steven Vandevelde <icid.asset@gmail.com>
2023-01-25 18:41:19 +0100

    Add code examples for working with events

--- src/events.ts
+++ src/events.ts
@@ -15,6 +15,16 @@ export type Events = {
  * Subscribe to events using `on` or `once` (multiple events are allowed),
  * and unsubscribe using `off`.
  *
+ * ```ts
+ * program.events.on("fileSystem:local-change", ({ path, root }) => {
+ *   console.log("The file system has changed locally 🔔")
+ *   console.log("Changed path:", path)
+ *   console.log("New data root CID:", root)
+ * })
+ *
+ * program.events.off("fileSystem:published")
+ * ```
+ *
  * More info on the [emittery Github readme](https://github.com/sindresorhus/emittery/tree/f0b3c2bf8dc985a7dde0e39607e30950394be54b#usage).
  */
 export type EventEmitter = Emittery<Events>
