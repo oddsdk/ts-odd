3733fd89432f4ac8e8553731d2dfd5391284acd3
Brian Ginsburg <gins@brianginsburg.com>
2023-02-27 13:29:07 -0800

    Add filesystem event listeners

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,13 +1,13 @@
 import type { AppInfo, AuthenticationStrategy, CID, FileSystemShortHands, ShortHands, Session } from "../index.js"
 import type { Maybe } from "../index.js"
+import type { DistinctivePath, Partition } from "../path/index.js"
 import * as Events from "../events.js"
 
 import { VERSION } from "../index.js"
 
-type Connection = {
-  extensionId: string | null
-  connected: boolean
-}
+
+// CREATE
+
 
 type Config = {
   auth: AuthenticationStrategy
@@ -17,6 +17,123 @@ type Config = {
   session: Maybe<Session>
   shorthands: ShortHands & { fileSystem: FileSystemShortHands & Events.ListenTo<Events.FileSystem> }
 }
+
+export async function create(config: Config): Promise<{
+  connect: (extensionId: string) => void
+  disconnect: (extensionId: string) => void
+}> {
+  let connection: Connection = { extensionId: null, connected: false }
+  let listeners: Listeners
+
+  return {
+    connect: async (extensionId: string) => {
+      await connect(extensionId, config)
+      connection = { extensionId, connected: true }
+
+      listeners = listen(connection, config)
+    },
+    disconnect: async (extensionId: string) => {
+      await disconnect(extensionId, config)
+      connection = { extensionId, connected: false }
+
+      stopListening(config, listeners)
+    }
+  }
+}
+
+
+
+// CONNECTION
+
+
+type Connection = {
+  extensionId: string | null
+  connected: boolean
+}
+
+async function connect(extensionId: string, config: Config): Promise<void> {
+  console.log("connect called with extension id", extensionId)
+
+  const state = await getState(config)
+
+  globalThis.postMessage({
+    id: extensionId,
+    type: "connect",
+    state
+  })
+}
+
+async function disconnect(extensionId: string, config: Config): Promise<void> {
+  console.log("disconnect called with extension id", extensionId)
+
+  const state = await getState(config)
+
+  globalThis.postMessage({
+    id: extensionId,
+    type: "disconnect",
+    state
+  })
+}
+
+
+
+// LISTENERS
+
+
+type Listeners = {
+  handleLocalChange: (params: { root: CID; path: DistinctivePath<[ Partition, ...string[] ]> }) => Promise<void>
+  handlePublish: (params: { root: CID }) => Promise<void>
+}
+
+function listen(connection: Connection, config: Config): Listeners {
+  async function handleLocalChange(params: { root: CID; path: DistinctivePath<[ Partition, ...string[] ]> }) {
+    const { root, path } = params
+    const state = await getState(config)
+
+    globalThis.postMessage({
+      id: connection.extensionId,
+      type: "filesystem",
+      state,
+      detail: {
+        type: "local-change",
+        root: root.toString(),
+        path
+      }
+    })
+  }
+
+  async function handlePublish(params: { root: CID }) {
+    const { root } = params
+    const state = await getState(config)
+
+    globalThis.postMessage({
+      id: connection.extensionId,
+      type: "filesystem",
+      state,
+      detail: {
+        type: "publish",
+        root: root.toString()
+      }
+    })
+  }
+
+  config.shorthands.fileSystem.on("local-change", handleLocalChange)
+  config.shorthands.fileSystem.on("publish", handlePublish)
+
+  return { handleLocalChange, handlePublish }
+}
+
+function stopListening(config: Config, listeners: Listeners) {
+  if (listeners) {
+    config.shorthands.fileSystem.removeListener("local-change", listeners.handleLocalChange)
+    config.shorthands.fileSystem.removeListener("publish", listeners.handlePublish)
+  }
+}
+
+
+
+// STATE
+
 
 type State = {
   app: {
@@ -32,79 +149,6 @@ type State = {
     accountDID: string | null
     agentDID: string
   }
-}
-
-export async function create(config: Config): Promise<{
-  connect: (extensionId: string) => void
-  disconnect: (extensionId: string) => void
-}> {
-  let connection: Connection = { extensionId: null, connected: false }
-
-  return {
-    connect: async (extensionId: string) => {
-      connect(extensionId)
-      connection = { extensionId, connected: true }
-
-      const state = await getState(config)
-      sendData(connection, state)
-
-      listen(connection, config)
-    },
-    disconnect: (extensionId: string) => {
-      disconnect(extensionId)
-      connection = { extensionId, connected: false }
-    }
-  }
-}
-
-function sendData(connection: Connection, state: State) {
-  if (connection.connected) {
-    globalThis.postMessage({
-      id: connection.extensionId,
-      type: "data",
-      data: state
-    })
-  }
-}
-
-function listen(connection: Connection, config: Config) {
-  const { shorthands } = config
-
-  shorthands.fileSystem.on("local-change", async ({ root, path }) => {
-    const state = await getState(config)
-
-    globalThis.postMessage({
-      id: connection.extensionId,
-      type: "data",
-      data: {
-        info: state,
-        type: "local-change",
-        root: root.toString(),
-        path
-      }
-    })
-
-  })
-
-
-}
-
-function connect(extensionId: string): void {
-  console.log("connect called with extension id", extensionId)
-
-  globalThis.postMessage({
-    id: extensionId,
-    type: "connect"
-  })
-}
-
-function disconnect(extensionId: string): void {
-  console.log("disconnect called with extension id", extensionId)
-
-  globalThis.postMessage({
-    id: extensionId,
-    type: "disconnect"
-  })
 }
 
 async function getState(config: Config): Promise<State> {
