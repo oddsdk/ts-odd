b322aab044753c622a21577950ef80fb7c16a764
Steven Vandevelde <icid.asset@gmail.com>
2023-07-10 14:24:00 +0200

    Improve iso-signatures integration

--- src/ucan/index.ts
+++ src/ucan/index.ts
@@ -1,6 +1,19 @@
 import * as Raw from "multiformats/codecs/raw"
 import * as Uint8arrays from "uint8arrays"
 import * as Ucans from "@ucans/core"
+
+// @ts-ignore
+import * as ECDSA from "iso-signatures/verifiers/ecdsa"
+
+// @ts-ignore
+import * as EDDSA from "iso-signatures/verifiers/eddsa"
+
+// @ts-ignore
+import * as RSA from "iso-signatures/verifiers/rsa"
+
+// @ts-ignore
+import { Resolver } from "iso-signatures/verifiers/resolver"
+
 import { DIDKey } from "iso-did/key"
 import { Ucan } from "@ucans/core"
 import { sha256 } from "multiformats/hashes/sha2"
@@ -9,7 +22,6 @@ import * as AgentDID from "../agent/did.js"
 import { Agent } from "../components.js"
 import { CID } from "../common/cid.js"
 import { BuildParams, Keypair } from "./types.js"
-import { rsa } from "../common/crypto.js"
 
 
 export { encode, encodeHeader, encodePayload, verify, parse, isExpired, isTooEarly } from "@ucans/core"
@@ -60,16 +72,16 @@ export function isSelfSigned(ucan: Ucan): boolean {
 }
 
 
-export async function isValid(crypto: Agent.Implementation, ucan: Ucan): Promise<boolean> {
-  const plug = await plugin(crypto)
-  const plugs = new Ucans.Plugins([ plug ], {})
+export async function isValid(agent: Agent.Implementation, ucan: Ucan): Promise<boolean> {
+  const plugs = await plugins(agent)
+  const jwtAlg = await agent.ucanAlgorithm()
 
   const signature = Uint8arrays.fromString(ucan.signature, "base64url")
   const signedData = Uint8arrays.fromString(ucan.signedData, "utf8")
 
   return !Ucans.isExpired(ucan)
     && !Ucans.isTooEarly(ucan)
-    && plugs.verifyIssuerAlg(ucan.payload.iss, plug.jwtAlg)
+    && plugs.verifyIssuerAlg(ucan.payload.iss, jwtAlg)
     && plugs.verifySignature(ucan.payload.iss, signedData, signature)
 }
 
@@ -85,24 +97,33 @@ export async function keyPair(agent: Agent.Implementation): Promise<Keypair> {
 }
 
 
-export async function plugin(agent: Agent.Implementation): Promise<Ucans.DidKeyPlugin> {
-  return {
-    prefix: new Uint8Array([ 0x85, 0x24 ]), // RSA
-    jwtAlg: await agent.ucanAlgorithm(),
-    verifySignature: (did: string, data: Uint8Array, sig: Uint8Array) => {
-      return rsa.verify({
-        message: data,
-        signature: sig,
-        publicKey: DIDKey.fromString(did).publicKey
-      })
-    }
-  }
+export async function plugins(agent: Agent.Implementation): Promise<Ucans.Plugins> {
+  return new Plugins([], {})
 }
 
 
-export async function plugins(agent: Agent.Implementation): Promise<Ucans.Plugins> {
-  return new Ucans.Plugins(
-    [ await plugin(agent) ],
-    {}
-  )
+class Plugins extends Ucans.Plugins {
+  verifyIssuerAlg(did: string, jwtAlg: string): boolean {
+    const dk = DIDKey.fromString(did)
+    return dk.alg === jwtAlg
+  }
+
+  async verifySignature(did: string, data: Uint8Array, sig: Uint8Array): Promise<boolean> {
+    const resolver = new Resolver(
+      {
+        ...ECDSA.verifier,
+        ...EDDSA.verifier,
+        ...RSA.verifier,
+      },
+      { cache: true }
+    )
+
+    const dk = DIDKey.fromString(did)
+
+    return resolver.verify({
+      sig,
+      data,
+      publicKey: dk.publicKey
+    })
+  }
 }
